57fa3c572b62bee1317c3e9758c0d675a3f422d8
Dustin Clifford <dclifford@sitesoftllc.net>
2026-03-15 19:52:09 -0400

    adding the ability to switch models through CL args

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+import os
+import sys
+
 import cv2
 from ultralytics import YOLO
 import supervision as sv
-import numpy as np
+
+MODEL_BASE_PATH='models/'
+MODELS = {
+    "DEFAULT": 'yolo26n.pt',
+    "YOLO26N": 'yolo26n.pt',
+    "YOLO11N": 'yolo11n.pt',
+    "YOLO8N": 'yolo8n.pt',
+
+}
 
 def initialize_camera(camera_index=0):
     cap = cv2.VideoCapture(camera_index)
     return cap
 
-def load_yolo_model(model_path="yolov8n.pt"):
-    model = YOLO(model_path)
+def load_yolo_model(model_key="DEFAULT"):
+    model_file = MODELS[model_key] if model_key in  MODELS else MODELS["DEFAULT"]
+
+    model = os.path.join(MODEL_BASE_PATH, model_file)
+    model = YOLO(model)
+
     return model
 
 def process_frame(frame, model, box_annotator):
@@ -28,9 +43,13 @@ def process_frame(frame, model, box_annotator):
 
 def main():
     cap = initialize_camera()
-    #model = load_yolo_model()
-    #model = load_yolo_model(model_path="yolo11n.pt")
-    model = load_yolo_model(model_path="yolo26n.pt")
+
+    if len(sys.argv) < 2 or sys.argv[1] not in MODELS:
+        model_key = "DEFAULT"
+    else:
+        model_key = sys.argv[1]
+    model = load_yolo_model(model_key)
+
     box_annotator = sv.BoxAnnotator(
         thickness=1,
         text_thickness=1,
@@ -46,7 +65,7 @@ def main():
 
         annotated_frame = process_frame(frame, model, box_annotator)
 
-        cv2.imshow("yolov8", annotated_frame)
+        cv2.imshow(f"The Pi Academy Object Detector - {model_key}", annotated_frame)
 
         if cv2.waitKey(20) == 27:
             break
